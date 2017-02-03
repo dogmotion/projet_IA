032ab2348d622a708d472c927bd49f2276ba3918
dogmotion <mael.leboulicaut63@laposte.net>
2017-02-03 08:18:27 +0100

    init commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print ("bonjour c'est moi , je suis ta nouvelle intelligence artifficielle qui n'a pas encore de nom, veut tu me donner un surnom")
+input("Entrez true ou flase si vous voulez le surnom!") 
+prenom = input("choisis mon prenom ")
+print("merci j'aime bien", prenom)
+nom = input("quel est ton nom?")
+print ("bonjour", nom) 
+age = int(input("quel âge as-tu?"))
+if (age < 20):  
+  print("Tu est trop jeune", nom ," dsl salut")
+else:
+  print("c bon tu peut avoir accès a moi",nom)
