c95ce3a339bfa2387e8f16ee461c13901c4925ec
Navet56 <evandib@gmail.com>
2017-02-03 17:59:26 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,12 +6,12 @@ nom = input("quel est ton nom?")
 print ("bonjour", nom) 
 age = int(input("quel âge as-tu?"))
 
-if (age < 20):  
+if (age < 18):  
   print("Tu est trop jeune", nom ," désolé salut")
 elif (age > 45):
   print(" je n'es pas été fait pour les vieux Désolé")
 else:
-  print("c'est bon tu peut avoir accès a moi", nom)
+  print("c'est bon tu peux me posez tes questions", nom)
   continuer = 0
   while continuer == 0:
     aDire = input("Que veux-tu me dire? : ")
@@ -37,15 +37,15 @@ else:
     
     elif aDire == ("comment tu t'appelle"):
       print("Amélia, ton assistante personnelle")
-    elif aDire == ("comment tu t'appelle"):
+    elif aDire == ("comment t'appelle tu"):
       print("Amélia, ton assistante personnelle")
-    elif aDire == ("comment tu t'appelle"):
+    elif aDire == ("c quoi ton nom"):
       print("Amélia, ton assistante personnelle")
-    elif aDire == ("comment tu t'appelle"):
+    elif aDire == ("comment tu tapelle"):
       print("Amélia, ton assistante personnelle")
-    elif aDire == ("comment tu t'appelle"):
+    elif aDire == ("comment tu tappelle"):
       print("Amélia, ton assistante personnelle")
-    elif aDire == ("comment tu t'appelle"):
+    elif aDire == ("comment tu t appelle"):
       print("Amélia, ton assistante personnelle")
       
     else:
