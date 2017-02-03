a66771ac2d7fe486006f4a20c9c1e7756a33946d
dogmotion <mael.leboulicaut63@laposte.net>
2017-02-03 18:07:36 +0100

    Update joke.py

--- resources/joke.py
+++ resources/joke.py
@@ -1,1 +1,7 @@
-
+ aDire = input("Que veux-tu me dire? : ")
+if aDire == ("dit moi une blague"):
+  print(" Connaissez-vous la blague de la tortue? Ne vous inquètez pas, elle n'est pas encore arrivée !")
+elif aDire == (" une blague")
+  print("Comment une blonde utilise-t-elle un éventail?Elle bouge la tête !")
+elif aDire == ("blague")
+ print("C'est deux putes qui se disputent")
