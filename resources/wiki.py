2ae8288c5e14c477428bd03d5c6c02e17003ac3c
dogmotion <mael.leboulicaut63@laposte.net>
2017-02-03 22:20:54 +0100

    Update wiki.py

--- resources/wiki.py
+++ resources/wiki.py
@@ -6,3 +6,8 @@ elif aDire == ("steve jobs")
   print("Steve Jobs est, avec Steve Wozniak, le cofondateur d’Apple. Ils sont tous deux considérés comme les pionniers de la micro-informatique pour avoir introduit l’ordinateur dans les foyers (bien avant l’avènement de l’IBM PC), puis pris conscience du potentiel du couple interface graphique / souris à la suite d’une visite avec une équipe de leur société au PARC de Xerox. Cette idée mènera à la commercialisation par la société Apple Computer du Macintosh, le premier ordinateur grand public profitant de ces innovations.Il est en 2009 la 43e fortune américaine.")
 elif aDire == ("guernica")
   print("Guernica est un tableau de Pablo Picasso. Cette oeuvre d'art a été créée en réponse au bombardement de Guernica par l'aviation allemande et italienne, à la demande des forces espagnoles nationalistes, le 26 avril 1937, durant la guerre civile espagnole.")
+elif aDire == ("résumé de eldorado")
+  print("Le travail du commandant Salvatore Piracci consiste a intercepter les immigrants qui arrivent par bateau en Sicile. Ils sont clandestins et attendent tout de leur nouvelle vie. Un jour, une femme arrivée deux années plus tôt sur l'île le retrouve. Elle lui raconte dans quelles conditions elle a voyagé et comment elle a perdu son bébé qui était avec elle sur le bateau. Plus tard, un homme qui se trouvait dans un bateau de clandestins qu'il vient d' intercepter lui demande son aide. Le commandant refuse.")
+elif aDire == ("bretagne")
+  print("C'est un petit pays ayant été envahi par un pays capitaliste la France")
+    
