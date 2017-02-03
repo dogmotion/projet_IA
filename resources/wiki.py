99548461ab3f60628919212fcf31cd4bb30cf84c
dogmotion <mael.leboulicaut63@laposte.net>
2017-02-03 17:51:47 +0100

    Ajout de 2 celebrité

--- resources/wiki.py
+++ resources/wiki.py
@@ -1,1 +1,7 @@
-
+while continuer == 0:
+    aDire = input("Que veux-tu me dire? : ")
+if aDire == ("quand est né steve jobs"):
+  print("le 24 février 1955, a San Fransisco, Californie, Etat Unis")
+elif aDire == ("steve jobs")
+  print("Steve Jobs est, avec Steve Wozniak, le cofondateur d’Apple. Ils sont tous deux considérés comme les pionniers de la micro-informatique pour avoir introduit l’ordinateur dans les foyers (bien avant l’avènement de l’IBM PC), puis pris conscience du potentiel du couple interface graphique / souris à la suite d’une visite avec une équipe de leur société au PARC de Xerox. Cette idée mènera à la commercialisation par la société Apple Computer du Macintosh, le premier ordinateur grand public profitant de ces innovations.Il est en 2009 la 43e fortune américaine.")
+elif aDire == ("Guernica est un tableau de Pablo Picasso. Cette oeuvre d'art a été créée en réponse au bombardement de Guernica par l'aviation allemande et italienne, à la demande des forces espagnoles nationalistes, le 26 avril 1937, durant la guerre civile espagnole.")
